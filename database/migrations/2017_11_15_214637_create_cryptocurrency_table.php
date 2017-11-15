a1ada364bb64a39ca82449e5bbe0cc24a8b00596
Pranay Marella <pranaylearn@gmail.com>
2017-11-15 16:17:40 -0600

    migrations and view changes

---
+++ database/migrations/2017_11_15_214637_create_cryptocurrency_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCryptocurrencyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cryptocurrency', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('currency_name')->comment('Expects name of Cryptocurrency');
+            $table->decimal('currency_price', 10, 2)->comment('Expects price of Cryptocurrency');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cryptocurrency');
+    }
+}
---
+++ database/migrations/2017_11_15_214716_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title')->comment('Expects title of news article');
+            $table->string('link')->comment('Expects link to news article');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
